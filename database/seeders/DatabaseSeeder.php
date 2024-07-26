a57bd31417afcfa8d17e5aa3fec6a340a0c206b2
Muhammad Arslan <mughalarslan996@gmail.com>
2024-07-26 16:38:45 +0500

    one to one chat and group chat

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $this->call([
+            UserSeeder::class,
         ]);
     }
 }
